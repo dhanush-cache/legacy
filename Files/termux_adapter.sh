7fc978697f95d61d67e3859b502b86bf73293add
Anonymous <anonymous@example.com>
2023-12-24 01:40:00 +0530

    Initial commit.

---
+++ Files/termux_adapter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if ! [ -f "/data/data/com.termux/files/usr/bin/sudo" ] || ! [ -f "/data/data/com.termux/files/usr/bin/python3-pip" ]; then
+    cp "./Files/bin/sudo" "/data/data/com.termux/files/usr/bin/sudo"
+    cp "./Files/bin/python3-pip" "/data/data/com.termux/files/usr/bin/python3-pip"
+    chmod +x "/data/data/com.termux/files/usr/bin/sudo" "/data/data/com.termux/files/usr/bin/python3-pip"
+fi
